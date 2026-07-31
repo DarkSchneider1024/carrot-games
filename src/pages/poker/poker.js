9210b39ea8640f4bb61fbb06bffa8adaceac006a
Carrot <carrot771024@gmail.com>
2026-07-31 15:56:12 +0800

    style: replace poker player avatars with AI generated Sanrio/Ghibli style cute head icons (Carrot, Rabbit, Cat, Bear)

--- src/pages/poker/poker.js
+++ src/pages/poker/poker.js
@@ -1,7 +1,7 @@
 /**
  * Texas Hold'em Poker Game Page (德州撲克對局頁面)
  *
- * Happy Hues Fresh Cute Theme with 3 AI Opponents & WebRTC P2P Multiplayer
+ * Happy Hues Fresh Cute Theme with AI Generated Avatars (Sanrio/Ghibli style)
  */
 
 import { navigate } from '../../router.js';
@@ -59,7 +59,9 @@ export async function renderPoker(container, params) {
 
           <!-- Opponent Seat 1 (Top Left) -->
           <div class="player-seat seat-top-left" id="seat-1">
-            <div class="poker-avatar">🐰</div>
+            <div class="poker-avatar">
+              <img src="/carrot-games/assets/images/avatar_rabbit.png" alt="兔兔" class="poker-avatar-img" />
+            </div>
             <div class="seat-info">
               <span class="seat-name">兔兔 (AI)</span>
               <span class="seat-chips" id="chips-1">$1000</span>
@@ -70,7 +72,9 @@ export async function renderPoker(container, params) {
 
           <!-- Opponent Seat 2 (Top Center) -->
           <div class="player-seat seat-top-center" id="seat-2">
-            <div class="poker-avatar">🐱</div>
+            <div class="poker-avatar">
+              <img src="/carrot-games/assets/images/avatar_cat.png" alt="貓咪" class="poker-avatar-img" />
+            </div>
             <div class="seat-info">
               <span class="seat-name">貓咪 (AI)</span>
               <span class="seat-chips" id="chips-2">$1000</span>
@@ -81,7 +85,9 @@ export async function renderPoker(container, params) {
 
           <!-- Opponent Seat 3 (Top Right) -->
           <div class="player-seat seat-top-right" id="seat-3">
-            <div class="poker-avatar">🐻</div>
+            <div class="poker-avatar">
+              <img src="/carrot-games/assets/images/avatar_bear.png" alt="熊熊" class="poker-avatar-img" />
+            </div>
             <div class="seat-info">
               <span class="seat-name">熊熊 (AI)</span>
               <span class="seat-chips" id="chips-3">$1000</span>
@@ -92,7 +98,9 @@ export async function renderPoker(container, params) {
 
           <!-- Main Local Player Seat (Bottom Center) -->
           <div class="player-seat seat-bottom" id="seat-0">
-            <div class="poker-avatar avatar-user">🥕</div>
+            <div class="poker-avatar avatar-user">
+              <img src="/carrot-games/assets/images/avatar_user.png" alt="玩家" class="poker-avatar-img" />
+            </div>
             <div class="seat-info">
               <span class="seat-name">你 (玩家)</span>
               <span class="seat-chips" id="chips-0">$1000</span>
